d3dec892d4ac3871107f5953768b1c307279a8f6
Leon <lmshimizu@gmail.com>
2021-10-07 20:29:15 -0700

    removed commented lines of code

--- weather_app.rb
+++ weather_app.rb
@@ -1,27 +1,5 @@
 require 'http'
 
-# puts "Enter any city and then your preferred units of measurement (imperial, metric, standard) and I'll tell you their current forecast:" 
-
-# location = gets.chomp
-# units = gets.chomp
-
-# weather = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=#{units}")
-
-# p weather.parse(:json)["name"]
-# p weather.parse(:json)["weather"][0]["description"]
-# p weather.parse(:json)["main"]["temp"]
-# p weather.parse(:json)["main"]["feels_like"]
-# p weather.parse(:json)["main"]["temp_min"]
-# p weather.parse(:json)["main"]["temp_max"]
-# p weather.parse(:json)["main"]["humidity"]
-# p weather.parse(:json)["wind"]["speed"]
-
-
-
-
-# output: current forecast - name, description, temp, feels like, min temp, max temp, humidity, wind speed
-
-
 def current_weather
 
   puts "Welcome! This is a weather app, if you'd like to continue, please type anything, if you'd like to cancel, please type 'done':"
@@ -52,7 +30,7 @@ def current_weather
 
     continue = gets.chomp
   end
-  
+
 end
 
 current_weather
