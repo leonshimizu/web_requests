e59f66fa0f9db657e62a36d10a12c02f5bf46ed3
Leon <lmshimizu@gmail.com>
2021-10-09 15:14:05 -0700

    created method to give random activity

--- bored.rb
+++ bored.rb
@@ -2,36 +2,29 @@ require 'http'
 
 def find_something_to_do
   
-  puts "Hey! If you're here, you must be bored, so let's find you something to do! If you'd like to continue, type anything, if you'd like to go back to doing nothing, type 'nothing':"
+  puts "Hey! If you're here, you must be bored, so let's find you something to do! If you'd like to continue, type anything, if you'd like to go back to doing nothing, type 'done':"
 
   continue = gets.chomp
 
+  while continue.downcase != "done"
+    puts "Great! So your activity of the day is ..."
+    link = HTTP.get("http://www.boredapi.com/api/activity?")
+    bored_link = link.parse(:json)
 
-  bored = HTTP.get("http://www.boredapi.com/api/activity?")
+    activity = bored_link["activity"]
+    type = bored_link["type"]
+    people = bored_link["participants"]
+    if people > 1
+      puts "#{activity}! This is a #{type} type of activity and requires a minimum of #{people} people."
+    else
+      puts "#{activity}! This is a #{type} type of activity and requires a minimum of #{people} person."
+    end
 
-  pp bored.parse(:json)["activity"]
-  pp bored.parse(:json)["type"]
-  pp bored.parse(:json)["participants"]
+    puts "If you'd like to get another random activity type anything, if you're happy with that activity, type 'nothing' or 'done'"
+
+    continue = gets.chomp
+  end
 
 end
 
-
-
-
-
-
-# attempted to use a recipe API
-# url = URI("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2300&diet=Intermittent%20Fasting&exclude=shellfish%2C%20olives")
-
-# http = Net::HTTP.new(url.host, url.port)
-# http.use_ssl = true
-# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-# request = Net::HTTP::Get.new(url)
-# request["x-rapidapi-host"] = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-# request["x-rapidapi-key"] = 'd790e13e0fmsha075308cccf68d1p15ea0bjsn55ff0484aea1'
-
-# response = http.request(request)
-# # p response.read_body
-
-# diet = HTTP.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2300&diet=Intermittent%20Fasting&exclude=shellfish%2C%20olives")
+find_something_to_do
