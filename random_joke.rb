fc0ef7bc665b62fc51f07f28040c3dd64995df03
Leon <lmshimizu@gmail.com>
2021-10-09 16:51:23 -0700

    made it loop until the user types 'done'

--- random_joke.rb
+++ random_joke.rb
@@ -1,48 +1,66 @@
 require 'http'
 
+def random_joke
 
-puts "Welcome to the random joke generator! Please select a category:"
-puts "Programming, Pun, Spook, Christmas, or All (Please only 1 selection)"
+  puts "Welcome to the random joke generator! To continue, type anything. To exit, type 'done':"
 
-choice = gets.chomp
-link = HTTP.get("https://v2.jokeapi.dev/joke/#{choice}")
-joke_link = link.parse(:json)
+  continue = gets.chomp
+
+  while continue.downcase != 'done'
+
+    puts "Please select one of the following categorys:"
+    puts "Programming, Pun, Spook, Christmas, or All (Please only 1 selection)"
+
+    choice = gets.chomp
+    link = HTTP.get("https://v2.jokeapi.dev/joke/#{choice}")
+    joke_link = link.parse(:json)
 
 
-if choice.downcase == "all"
-  link = HTTP.get("https://v2.jokeapi.dev/joke/Programming,Pun,Spooky,Christmas")
-  joke_link = link.parse(:json)
-  joke = joke_link["joke"]
-  category = joke_link["category"]
-  setup = joke_link["setup"]
-  delivery = joke_link["delivery"]
-  type = joke_link["type"]
-  if joke_link["type"] == "single"
-    puts "=" * 50
-    puts "Here's a #{category} joke:"
-    puts joke
-  elsif joke_link["type"] == "twopart"
-    puts "^" * 50
-    puts "Here's a #{category} joke (it's a two part joke):"
-    puts setup
-    gets.chomp
-    puts delivery
+    if choice.downcase == "all"
+      link = HTTP.get("https://v2.jokeapi.dev/joke/Programming,Pun,Spooky,Christmas")
+      joke_link = link.parse(:json)
+      joke = joke_link["joke"]
+      category = joke_link["category"]
+      setup = joke_link["setup"]
+      delivery = joke_link["delivery"]
+      type = joke_link["type"]
+      if joke_link["type"] == "single"
+        puts "-" * 50
+        puts "Here's a #{category} joke:"
+        puts joke
+      elsif joke_link["type"] == "twopart"
+        puts "-" * 50
+        puts "Here's a #{category} joke (it's a two part joke):"
+        puts setup
+        gets.chomp
+        puts delivery
+      end
+    else 
+      link = HTTP.get("https://v2.jokeapi.dev/joke/#{choice}")
+      joke_link = link.parse(:json)
+      joke = joke_link["joke"]
+      category = joke_link["category"]
+      setup = joke_link["setup"]
+      delivery = joke_link["delivery"]
+      type = joke_link["type"]
+      if joke_link["type"] == "single"
+        puts "-" * 50
+        puts "Here's a #{category} joke:"
+        puts joke
+      elsif joke_link["type"] == "twopart"
+        puts "-" * 50
+        puts "Here's a #{category} joke (it's a two part joke):"
+        puts setup
+        gets.chomp
+        puts delivery
+      end
+    end
+    puts "-" * 50
+    puts "To generate another joke, type anything. To exit, type 'done':"
+
+    continue = gets.chomp
   end
-else 
-  link = HTTP.get("https://v2.jokeapi.dev/joke/#{choice}")
-  joke_link = link.parse(:json)
-  joke = joke_link["joke"]
-  category = joke_link["category"]
-  setup = joke_link["setup"]
-  delivery = joke_link["delivery"]
-  type = joke_link["type"]
-  if joke_link["type"] == "single"
-    puts "Here's a #{category} joke:"
-    puts joke
-  elsif joke_link["type"] == "twopart"
-    puts "Here's a #{category} joke (it's a two part joke):"
-    puts setup
-    gets.chomp
-    puts delivery
-  end
+
 end
+
+random_joke
