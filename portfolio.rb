e0a0b068574bff223be0f60d3c97f7dcaab96374
Leon <lmshimizu@gmail.com>
2021-10-09 19:47:01 -0700

    added bored.rb to portfolio.rb

--- portfolio.rb
+++ portfolio.rb
@@ -1,6 +1,6 @@
 puts "Welcome! Here are all the main applications I've created:"
 puts "-" * 50
-puts "Random Joke Generator and Current Weather"
+puts "Random Joke Generator, Current Weather, Random Activity Generator"
 puts "Please select one:"
 
 selection = gets.chomp
@@ -9,4 +9,6 @@ if selection.downcase == "current weather"
   require_relative 'weather_app.rb'
 elsif selection.downcase == "random joke generator"
   require_relative 'random_joke.rb'
+elsif selection.downcase == "random activity generator"
+  require_relative 'bored.rb'
 end
